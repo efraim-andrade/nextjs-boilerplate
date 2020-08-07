6dcc312fbac1a334656e30b83108b95fd155f5d9
Efraim Andrade <efraim.dev@gmail.com>
2020-08-07 19:23:51 -0300

    feat: 🎸 eslint/prettier
    added the tools on the project

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// import { Container } from './styles';
+
+const pages: React.FC = () => {
+  return (
+    <div className="container">
+      <p>nextzao</p>
+    </div>
+  )
+}
+
+export default pages
