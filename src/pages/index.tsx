ba22822343d969b8e568579e4cba1dab25c7e571
Efraim Andrade <efraim.dev@gmail.com>
2020-08-07 20:20:49 -0300

    feat: styled-components
    added styled components and some head configs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 
 // import { Container } from './styles';
+import Main from '~/components/Main/index'
 
 const pages: React.FC = () => {
-  return (
-    <div className="container">
-      <p>nextzao</p>
-    </div>
-  )
+  return <Main />
 }
 
 export default pages
