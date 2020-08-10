020a82ce31b5d2ddf3827039c4c63b0de1d492fe
Efraim Andrade <efraim.dev@gmail.com>
2020-08-10 00:55:26 -0300

    fix: 🐛 jest
    fix the coverage in wrong files

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,16 @@
 module.exports = {
   testEnvironment: 'jsdom',
-  testPathIgnorePatterns: [
-    '/node_modules/',
-    '/.next/',
-    'pages/_app.tsx',
-    'pages/_document.tsx'
-  ],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/*.stories.tsx'],
+  collectCoverageFrom: [
+    'src/pages/**/*.tsx',
+    'src/components/**/*.tsx',
+    'src/utils/**/*.ts',
+    'src/hooks/**/*.ts',
+    '!src/**/*.stories.tsx',
+    '!src/pages/_app.tsx',
+    '!src/pages/_document.tsx'
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/']
 }
