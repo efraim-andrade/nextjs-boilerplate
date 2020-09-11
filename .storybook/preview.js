144cf68d344a128872c10ffa307fb7649fe6d854
Efraim Andrade <efraim.dev@gmail.com>
2020-09-10 23:00:48 -0300

    feat: removing center storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,25 +3,12 @@ import styled, { ThemeProvider } from 'styled-components'
 import theme from 'styles/theme'
 import GlobalStyles from 'styles/global'
 
-const Center = styled.div`
-  width: 100%;
-  height: 100vh;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  background: #e5e5e5;
-`
-
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={theme}>
-      <Center>
-        <GlobalStyles />
+      <GlobalStyles />
 
-        <Story />
-      </Center>
+      <Story />
     </ThemeProvider>
   )
 ]
